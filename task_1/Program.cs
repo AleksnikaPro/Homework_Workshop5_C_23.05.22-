f38470cf55105956c0ae5eba0053947342fcd4cf
Aleksandra Popova <alexnika@bk.ru>
2022-05-27 15:06:40 +0300

    added task_3

---
+++ task_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine("Давайте найдем точку пересечения двух прямых!");
+Console.WriteLine("Введите значение k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b2: ");
+double b2 = double.Parse(Console.ReadLine());
+
+double x = " ";
+double y = " ";
+double сoordinateX = x;
+double сoordinateY = y;
+
+
+ y = k1 * x + b1;
+ y = k2 * x + b2;
+
+ y - y = k1 * x + b1 - (k2 * x + b2);
+ y = k2 * x + b2;
+
+ 
+
+  
+ 
+ Console.Write ($"Точка пересечения двух прямых имеет координаты {(x, y)};
+
+
+
+
---
+++ task_3/Program.cs
@@ -0,0 +1,31 @@
+﻿//3. Написать программу копирования массива 
+
+Console.Clear();
+Console.WriteLine("Наш массив:");
+
+int [] myArray = new int [7];
+
+for (int i = 0; i < myArray.Length; i++) 
+{
+    myArray [i] = new Random().Next(0,10); 
+}
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + " ");
+}
+Console.WriteLine();
+
+Console.WriteLine();
+Console.WriteLine("Копия нашего массива:");
+int [] myCopyArray = new int [myArray.Length];
+for (int i = 0; i <myArray.Length; i++)
+{
+   myCopyArray[i] = myArray [i];
+   Console.Write(myCopyArray[i] + " ");
+
+}
+Console.WriteLine();
+
+
+
+
