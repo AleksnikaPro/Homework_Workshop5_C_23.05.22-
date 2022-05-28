2b14478c53b556446b04dcf5627c969d9c38f153
Aleksandra Popova <alexnika@bk.ru>
2022-05-28 19:47:14 +0300

    added changes in task_1

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,32 +1,19 @@
-﻿// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+﻿// 1.Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
 
 Console.WriteLine("Давайте найдем точку пересечения двух прямых!");
 Console.WriteLine("Введите значение k1: ");
-double k1 = double.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine()?? "0");
 Console.WriteLine("Введите значение b1: ");
-double b1 = double.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine()?? "0");
 Console.WriteLine("Введите значение k2: ");
-double k2 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine()?? "0");
 Console.WriteLine("Введите значение b2: ");
-double b2 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine()?? "0");
 
-double x = " ";
-double y = " ";
-double сoordinateX = x;
-double сoordinateY = y;
+double x=(b2-b1)/(k1-k2); 
+double y=k1*(b2-b1)/(k1-k2)+b1;
+Console.Write($"Точка пересечения двух прямых имеет координаты {(x, y)}");
 
-
- y = k1 * x + b1;
- y = k2 * x + b2;
-
- y - y = k1 * x + b1 - (k2 * x + b2);
- y = k2 * x + b2;
-
- 
-
-  
- 
- Console.Write ($"Точка пересечения двух прямых имеет координаты {(x, y)};
 
 
 
